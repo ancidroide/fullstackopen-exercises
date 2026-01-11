6971f3bb658aa2eb4f93ece962bd3c4cfea088d3
ancidroide <ancipolla@gmail.com>
2026-01-11 23:48:35 +0100

    1.4 done

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,67 +1,60 @@
-// First of all: component definition
-
 const Header = (props) => {
   return (
     <h1>{props.course}</h1>
   )
 }
 
-const Part = ( {part} ) => {
+const Part = ( {name, exercises} ) => {
   return (
-    <p>Course: {part.name}, Exercises: {part.exercises}</p>
+    <p>Course: {name}, Exercises: {exercises}</p>
   )
 }
 
-const Content = (props) => {
+const Content = ( {parts} ) => {
   return (
     <div>
-      <Part part={props.part1}/>
-      <Part part={props.part2}/>
-      <Part part={props.part3}/>
-
+      {parts.map((part) => (
+        <Part name={part.name} exercises={part.exercises} />
+      ))}
     </div>
   )
 }
 
-const Total = (props) => {
-  const sum = props.part1.exercises + props.part2.exercises + props.part3.exercises
+const Total = ( {parts})  => {
+  const sum = parts[0].exercises + parts[1].exercises + parts[2].exercises
   return (
     <p>Total number of exercises: {sum}</p>
   )
 }
 
 
+
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
+
 
   return (
     <div>
       <Header course={course} />
-      <Content
-        part1={part1}
-        part2={part2}
-        part3={part3}
-      />
-      <Total
-        part1={part1}
-        part2={part2}
-        part3={part3}
-      />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   )
 }
 
-// 3. Export
+
 export default App
