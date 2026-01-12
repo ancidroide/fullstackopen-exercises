a2d126ae988ac9cacf702c8d34fa4701c1bacaae
ancidroide <ancipolla@gmail.com>
2026-01-12 10:55:35 +0100

    1.5 done

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,6 +1,6 @@
-const Header = (props) => {
+const Header = ( {course} ) => {
   return (
-    <h1>{props.course}</h1>
+    <h1>{course.name}</h1>
   )
 }
 
@@ -14,7 +14,7 @@ const Content = ( {parts} ) => {
   return (
     <div>
       {parts.map((part) => (
-        <Part name={part.name} exercises={part.exercises} />
+        <Part key={part.name} name={part.name} exercises={part.exercises} />
       ))}
     </div>
   )
@@ -30,28 +30,29 @@ const Total = ( {parts})  => {
 
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
-
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
       <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
