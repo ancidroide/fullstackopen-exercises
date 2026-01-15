1e1ee534e182b27c8634252dc17db77a7bb90b94
ancidroide <ancipolla@gmail.com>
2026-01-15 23:00:01 +0100

    2.6 done

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,56 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', id: 1}
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const handleSubmit = (event) => {
+    event.preventDefault() 
+    
+    // create react ID for the new person
+      // if persons array not empty --> create an array with any id (.map())
+      // and take as id the max id in this array; if array is empty take 1 as id
+    const personId = persons.length > 0 ? Math.max(...persons.map(p => p.id)) + 1 : 1;
+    const newPerson = {
+      name: newName,
+      id: personId
+    }
+    
+    // add the newName to the PHONEBOOK (persons obj)
+    setPersons([
+      ...persons,
+      newPerson
+    ])
+
+    // reset the newName input field
+    setNewName('')
+  } 
+
+  const handleInput = (event) => {
+    setNewName(event.target.value)
+  }
+
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          name: <input value={newName} onChange={handleInput}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+
+      {persons.map(person => 
+        <div key={person.id}>{person.name}</div>
+        )}
+    </div>
+  )
+}
+
+export default App
