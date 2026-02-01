2d34d15232c7c3a1f8508925811c57b5e2f94193
ancidroide <ancipolla@gmail.com>
2026-02-01 10:08:12 +0100

    4.4 done

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,13 +2,14 @@ const dummy = (blogs) => {
     return 1
 }
 
-const totalLikes = () {
-    
-}
+const totalLikes = (blogs) => 
+    blogs.reduce((sum, blog) => sum + blog.likes, 0)
+
 
 
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
 
 
