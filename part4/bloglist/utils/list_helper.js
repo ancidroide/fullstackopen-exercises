93b01188c89a2278fa49f79ef3e1b3a5cd7ace5c
ancidroide <ancipolla@gmail.com>
2026-02-02 00:09:39 +0100

    4.6-4.7 done

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,3 +1,5 @@
+const _ = require('lodash')
+
 const dummy = (blogs) => {
     return 1
 }
@@ -11,8 +13,35 @@ const favoriteBlog = (blogs) => {
     })
 }
 
+const mostBlogs = (blogs) => {
+    const counts = _.countBy(blogs, 'author')
+    const authorCounts = _.map(counts, (val, key) => {
+        return {
+            author: key,
+            blogs: val,
+        }
+    })
+
+    return _.maxBy(authorCounts, 'blogs') 
+    
+    
+}
+
+const mostLikes = (blogs) => {
+    const counts = _.groupBy(blogs, 'author')
+    const likesCount = _.map(counts, (val, key) => {
+        return {
+            author: key,
+            likes: totalLikes(val)
+        }
+    })
+    return _.maxBy(likesCount, 'likes')
+}
+
 module.exports = {
     dummy,
     totalLikes,
     favoriteBlog,
+    mostBlogs,
+    mostLikes,
 }
