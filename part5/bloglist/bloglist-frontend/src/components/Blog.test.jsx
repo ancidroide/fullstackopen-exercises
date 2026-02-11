1ec238c8825bccecf913aad4f35607705c0a45ab
ancidroide <ancipolla@gmail.com>
2026-02-11 14:25:36 +0100

    part 5 --> 5.14 done

--- part5/bloglist/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 test('renders title and author', () => {
@@ -20,6 +21,7 @@ test('renders title and author', () => {
 
   render(<Blog blog={blog} updateBlog={() => {}} user={user} deleteBlog={() => {}} />)
   
+  // Exercise 5.13 (verify title and author IN the document && url and likes NOT in the document)
   const titleElement = screen.getByText((content) => content.includes('title test'))
   expect(titleElement).toBeInTheDocument()
 
@@ -31,5 +33,34 @@ test('renders title and author', () => {
 
   const likesElement = screen.queryByText('likes:')
   expect(likesElement).toBeNull()
+})
 
+// 5.15 (verify button view shows url && likes)
+test('shows URL and likes when view button clicked', async () => {
+  const blog = {
+    title: 'title test',
+    author: 'author test',
+    url: 'http://test.com',
+    likes: 0,
+    user: {
+      id: '123',
+      name: 'Test User'
+    }
+  }
+
+  const user = {
+    id: '123',
+    name: 'Test User'
+  }
+  render(<Blog blog={blog} updateBlog={() => {}} user={user} deleteBlog={() => {}} />)
+
+  const viewButton = screen.getByText('view')
+  await userEvent.click(viewButton)
+
+  
+  const urlElement = screen.getByText((content) => content.includes('http://test.com'))
+  expect(urlElement).toBeInTheDocument()
+
+  const likesElement = screen.getByText((content) => content.includes('likes:'))
+  expect(likesElement).toBeInTheDocument()
 })
