533f49211717b7c2372d1cc38041f30e558f5f87
ancidroide <ancipolla@gmail.com>
2026-02-12 15:08:34 +0100

    part 5 --> 5.18 done

--- part5/bloglist/bloglist-frontend/tests/blog_app.spec.js
+++ part5/bloglist/bloglist-frontend/tests/blog_app.spec.js
@@ -1,8 +1,22 @@
 import { test, expect } from '@playwright/test'
 
+
 test.describe('Blog app', () => {
-  test.beforeEach(async ({ page }) => {
-    // Navigate to the application
+  test.beforeEach(async ({ page, request }) => {
+    // empty db
+    await request.post('http://localhost:3001/api/testing/reset')
+
+    // create a new User and add it to DB
+    const newUser = {
+        username: 'testuser',
+        password: 'testpassword',
+        name: 'Test User'
+    }
+
+    const response = await request.post('http://localhost:3001/api/users', {
+    data: newUser
+    })
+    // connect to bloglist app page
     await page.goto('http://localhost:5173')
   })
 
@@ -22,5 +36,30 @@ test.describe('Blog app', () => {
     // Step 4: Verify login button exists
     const loginButton = page.getByRole('button', { name: 'login' })
     await expect(loginButton).toBeVisible()
+  })
+
+  test.describe('Login', () => {
+    test('succeeds with correct credentials', async ({ page }) => {
+        // fill inputs
+        await page.getByPlaceholder('username').fill('testuser')
+        await page.getByPlaceholder('password').fill('testpassword')
+
+        // click login btn
+        await page.getByRole('button', { name: 'login '}).click()
+
+        // verify successfull login
+        await expect(page.locator('.notification.success')).toContainText('Test User logged in successfully')
+    })
+
+    test('fails with wrong credentials', async ({ page }) => {
+        await page.getByPlaceholder('username').fill('testuser')
+        await page.getByPlaceholder('password').fill('wrongpassword')
+        await page.getByRole('button', { name: 'login' }).click()
+
+        await expect(page.getByText('wrong username or password')).toBeVisible()
+        await expect(page.getByPlaceholder('username')).toBeVisible()
+    })
+
+
   })
 })
